2875e99eca49fa40702c986be0704f15534f553b
Nombulelo <buli.hatta@gmail.com>
2023-05-24 03:41:55 -0700

    Print the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,15 +10,14 @@ int print_last_digit(int num)
 {
 	int last_dig;
 
-	if (num <= 0)
+	if (num < 0)
 	{
-		last_dig = -num % 10;
+		last_dig = (-num % 10);
 	}
 	else
 	{
 		last_dig = num % 10;
 	}
-
 	_putchar(last_dig + '0');
 
 	return (last_dig);
