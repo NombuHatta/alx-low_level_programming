61b801e74f5cf2ed13bdfcac8c3e679361aac04e
Nombulelo <buli.hatta@gmail.com>
2023-06-01 03:07:10 -0700

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,18 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i;
 
-	while (s1[i] == s2[i])
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (s1[i] == '\0')
-			return (0);
-		i++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	}
 
-	return(s1[i] - s2[i]);
+	if (s1[i] != '\0')
+		return (s1[i]);
+	else if (s2[i] != '\0')
+		return (-s2[i]);
+
+	return (0);
 }
