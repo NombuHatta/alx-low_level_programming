5676cbd9303ec4dfb1108f9fdd9de64a1fb5bc96
Nombulelo <buli.hatta@gmail.com>
2023-06-20 13:38:26 -0700

    New struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,27 @@
-typedef struct dog
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - Dogs information
+ * @name: Pointer to the name of the dog
+ * @age: age of the dog
+ * @owner: Pointer to the owner of the dog
+ *
+ * Description: Longer description
+ */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
