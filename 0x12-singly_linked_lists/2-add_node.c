eb1979d01c764acfdb16f0cbf2db4b33900b5445
Nombulelo <buli.hatta@gmail.com>
2023-07-04 09:49:38 -0700

    Add a new node at the beginning of a list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,37 +3,35 @@
 #include <string.h>
 
 /**
- * add_nobe - adds a new nobe at the begining of the list
+ * add_node - adds a new nobe at the begining of the list
  * @head: pointer to the head of th list
  * @str: string to be added to the list
  *
  * Return: The address of the element, or NULL if it failed
  */
 
-list_t *add_nobe(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_nobe;
+	list_t *new_node;
 
-	if (str == NULL)
+	if (head == NULL || str == NULL)
 		return (NULL);
 
-	new_nobe = malloc(sizeof(list_t));
-	if (new_nobe == NULL)
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
+		free(new_node);
 		return (NULL);
 	}
 
-	new_nobe->str = strdup(str);
-	if (new_nobe->str == NULL)
-	{
-		free(new_nobe);
-		return (NULL);
-	}
 
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
 
-	new_nobe->len = strlen(str);
-	new_nobe->next = *head;
-	*head = new_nobe;
-
-	return (new_nobe);
+	return (new_node);
 }
