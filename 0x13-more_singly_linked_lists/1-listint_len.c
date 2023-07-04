d97b4687d595a62a16aad6c86b61eed422712875
Nombulelo <buli.hatta@gmail.com>
2023-07-04 12:05:58 -0700

    Return the number of elements in a linked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -5,13 +5,13 @@
  * listint_len - Return the number of elements in a linked list
  * @h: Pointer to the head of the linker list
  *
- * @Return: The number of elements in the linker list
+ * Return: The number of elements in the linker list
  */
 
 size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
-	
+
 	while (h != NULL)
 	{
 		count++;
