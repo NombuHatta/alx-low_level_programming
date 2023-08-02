70366dce69a42a8654662844e0cba5af5dfa091e
NombuHatta <buli.hatta@gmail.com>
2023-08-02 01:44:24 -0700

    Puts with recursion[A

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,10 +9,18 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (s == NULL)
 	{
+		return;
+	}
+
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
 		_putchar(*s);
 		_puts_recursion(s + 1);
 
-	}
 }
