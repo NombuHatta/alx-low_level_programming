8a38e302af149e3f744f115297e27df3ef9e54a2
NombuHatta <buli.hatta@gmail.com>
2023-08-29 10:23:32 -0700

    freesa linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * free_listint2 - Frees a listint_t and sets the head to NULL
+ * @head: Pointer to the pointer to the head of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head, *temp;
+	if (head == NULL)
+		return;
+
+	while (current != NULL)
+	{
+		temp = current;
+
+		current = current->next;
+		free(temp);
+	}
+	*head = NULL;
+}
