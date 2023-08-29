9f2e8e5f2f6caaf16c05ac583c77bfe8f8530f4b
NombuHatta <buli.hatta@gmail.com>
2023-08-29 08:56:36 -0700

    Quick fix

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,7 +5,7 @@
 /**
  * struct listint_s - singly linked list
  * @n: integer
- * @nextt: points to the next node
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
  *
